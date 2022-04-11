6af005038712fe0656a4f85db11e114d57977675
Catrina Alexandru <ovidiualex.catrina@gmail.com>
2022-04-11 17:11:57 +0300

    other commit

--- app.js
+++ app.js
@@ -4,11 +4,3 @@ import Data from "./Api.js";
 
 new Home();
 
-/*let a1= new Apartament(8,"Semidecomandat",3,1950,"Neinchiriat");
-
-console.log(ca.adaugareApartamentNou(a1));*/
-
-//let ca= new ControlApartament();
-
-//new ControlApartament();
-//new Home();*/
